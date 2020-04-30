1df0b9004c62626bc2fea1d263a0693ad23bd082
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-04-30 16:25:56 +0100

    clean_migration_by_UKborn() finished and example doc

--- R/clean_migration_by_UKborn.R
+++ R/clean_migration_by_UKborn.R
@@ -1,21 +1,70 @@
 
 #' clean_migration_by_UKborn
 #'
+#' @param dir_path
+#' @param input_file_name
+#' @param rtn
+#' @param save_to_file
+#' @param save_path
+#' @param save_format
+#'
+#' @import readxl, dplyr, reshape2
 #' @return
 #' @export
 #'
-clean_migration_by_UKborn <- function() {
-  
-readxl::read_xls(IPS2019)
-readxl::read_xls(ons_internation_migration)
+clean_migration_by_UKborn <- function(dir_path = here::here("rawdata"),
+                                      input_file_name = "2.03ltimcountryofbirth2004to2018.xls",
+                                      rtn = TRUE,
+                                      save_to_file = TRUE,
+                                      save_path = here::here("output_data"),
+                                      save_format = "csv") {
 
-IPS
-inflow by UK born
-outflow by UK born
+  inflow <- readxl::read_xls(paste(dir_path, input_file_name, sep = "/"),
+                             sheet = "Table 2.03",
+                             range = "A17:I40")
 
-ONS
-inflow by UK born
-outflow by UK born
+  outflow <- readxl::read_xls(paste(dir_path, input_file_name, sep = "/"),
+                              sheet = "Table 2.03",
+                              range = "A17:I58")
+  inflow <-
+    inflow %>%
+    select_if(~!(all(is.na(.)) | all(. == ""))) %>%  # remove empty columns
+    select_at(vars(-contains(".."))) %>%
+    mutate(Year = as.numeric(Year)) %>%
+    filter(!is.na(Year)) %>%
+    rename(UKborn = `United Kingdom`,
+           NonUKborn = `Non-United Kingdom`,
+           total = `All countries`) %>%
+    mutate(UKborn = as.numeric(UKborn),
+           NonUKborn = as.numeric(NonUKborn),
+           total = as.numeric(total)) %>%
+    mutate(pUKborn = UKborn/total,
+           pNonUKborn = NonUKborn/total)
 
-save
+  outflow <-
+    outflow %>%
+    select_if(~!(all(is.na(.)) | all(. == ""))) %>%
+    select_at(vars(-contains(".."))) %>%
+    mutate(Year = as.numeric(Year)) %>%
+    rename(UKborn = `United Kingdom`,
+           NonUKborn = `Non-United Kingdom`,
+           total = `All countries`) %>%
+    filter(total < 0) %>%
+    mutate(UKborn = as.numeric(UKborn),
+           NonUKborn = as.numeric(NonUKborn),
+           total = as.numeric(total)) %>%
+    mutate(pUKborn = UKborn/total,
+           pNonUKborn = NonUKborn/total)
+
+  if (save_to_file) {
+    write.csv(inflow, file = here::here("output_data", "inflow.csv"))
+    save(inflow, file = here::here("data", "inflow.RData"))
+
+    write.csv(outflow, file = here::here("output_data", "outflow.csv"))
+    save(outflow, file = here::here("data", "outflow.RData"))
+  }
+
+  if (rtn)
+    return(list(inflow = inflow,
+                outflow = outflow))
 }
